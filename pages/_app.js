fe4fb5c32d64ca11e854d3d39fa09671d533c2b2
asepterbakarbakar-droid <asepterbakarbakar@gmail.com>
2025-10-15 14:20:15 +0700

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -2,19 +2,5 @@ import '../styles/globals.css'
 import { useEffect } from 'react'
 
 export default function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    // Initialize database on app start
-    const initDB = async () => {
-      try {
-        const { initDatabase } = await import('../lib/database')
-        await initDatabase()
-      } catch (error) {
-        console.error('Failed to initialize database:', error)
-      }
-    }
-    
-    initDB()
-  }, [])
-
   return <Component {...pageProps} />
 }
